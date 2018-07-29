d1e15f09a727cc7355dfa8ef7201968aba612497
Rani Abu Sultan <raniabusultan@gmail.com>
2018-07-29 09:26:14 +0300

    Sets - Initial commit

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 Typescript playground
 
-## Development server
+## Sets
 
-Run `npm start` for a dev server. Navigate to `http://localhost:9000/`. The app will automatically reload if you change any of the source files.
-
-## TypeScript compilation in watch mode
-Run `tsc --watch` for watch in terminal.
+* A Set is a data structure which contains a list of values that are unique.
+* You can add strings, numeric and objects to a set using the add method.
+* Objects added to the set are not converted to strings.
+* We can initialize values using an array during the creation of a set.
+* we can chain the add method to add elements to a set.
+* To check for existence of a value in a set we use the has method.
+* To delete a value from a set we use the delete method.
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-console.log('hi');
+let mySet = new Set();
 
-import {Car} from './Car';
+mySet.add("Hello!");
+mySet.add(1);
+mySet.add(1); // Adding the same value to the set is ignored. Because a set can only contain unique values.
 
-const car: Car = new Car();
+console.log(mySet.size);
 
-car.go();
+let ob1 = {};
+let ob2 = {};
+mySet.add(ob1);
+mySet.add(ob2);
 
+console.log(mySet.size);
+
+let newSet = new Set([1,2,3,4, ob1, ob2])
+
+console.log("size of newSet: " + newSet.size);
+console.log("Has value 1? " + newSet.has(1));
+console.log("Has value 5? " + newSet.has(5));
+console.log("Has ob1? " + newSet.has(ob1));
+console.log("Deleting value 1 ... " + newSet.delete(1));
+console.log("size of newSet after deleting: " + newSet.size);
+
+let chainSet = new Set().add("Hello").add("World");
+
+console.log(chainSet.size);

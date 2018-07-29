abab2390d1613efbee4df8fd2f5351ade186c39f
Rani Abu Sultan <raniabusultan@gmail.com>
2018-07-29 18:32:20 +0300

    WeakMap - Initial Commit

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 Typescript playground
 
-## Weak Sets
+## Weak Maps
 
-* In a strong set, if we assign null to an object, a referance to the object still exists in the set. And it can be easily retrieved like this: `obj = [...mySet][0]`.
-* To avoid memory leaks, we would prefer that the reference in a set to disappear as well. For this purpose, we use the `WeakSet()` instead of `Set()`.
-* Weak sets can only store object references.
-* In a weak set, if we assign null to an object, a reference to the object is no longer accessible.
-* The advantage of weak set over a strong set is that memory is handled properly. So, if we have only to track the references to objects and nothing more than that then a weak set should be preferred over a strong set.
+* The key can only be object and the object referances are weak meaning they don't interfere with the garbage collection,
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,5 @@
-console.log(
-`
-strong Set
---------------
-`
-);
-let strongSet = new Set();
-let obj1 = {};
-strongSet.add(obj1);
-console.log("Size of strongSet after adding obj is: " + strongSet.size);
-obj1 = null;
-console.log("Size of strongSet after setting obj = null remaines the same because a referance to the object still exists in the set: " + strongSet.size);
-console.log("If you check and see if it has the obj? " + strongSet.has(obj1));
-obj1 = [...strongSet][0]; //retrieving the obj
-console.log("Checking after retrieving the obj ..." + strongSet.has(obj1));
-
-console.log(
-`
-Weak Set
---------------
-`
-);
-
-let weakSet = new WeakSet();
-let obj2 = {};
-weakSet.add(obj2);
-console.log("If you check and see if it has the obj2? " + weakSet.has(obj2));
-obj2 = null;
-console.log("If you check and see if it has the obj2? " + weakSet.has(obj2));
+let myMap = new WeakMap();
+let ob1 = {};
+myMap.set(ob1, "Hello World!");
+console.log(myMap.get(ob1));
+ob1 = null;

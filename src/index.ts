b325210eae43b7f6bd6bd577d6eb7a3ac518e471
Rani Abu Sultan <raniabusultan@gmail.com>
2018-07-29 18:22:39 +0300

    forEach - Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
-console.log('hi');
+console.log(`
+forEach - Array
+-------------------------------------------
+`);
+var numbers = [2,4,6,8];
+numbers.forEach(arrayFunction);
+function arrayFunction(element,index,array){
+    console.log("arr[" + index + "]= "+ element);
+    console.log(numbers === array);
+}
+console.log(`
 
-import {Car} from './Car';
+forEach - Map
+-------------------------------------------
+`);
+let myMap = new Map([
+    ["fname","Rani"],
+    ["lname","Abu Sultan"]
+]);
+myMap.forEach(mapFunction);
+function mapFunction(value, key, callingMap){
+    console.log(key + " " + value);
+    console.log(myMap === callingMap);
+    
+}
+console.log(`
 
-const car: Car = new Car();
-
-car.go();
-
+forEach - Set
+-------------------------------------------
+`);
+let mySet = new Set([1,2,3]);
+mySet.forEach(setFunction);
+function setFunction(value,key,callingSet){
+    console.log(key + " " + value);
+    console.log(myMap === callingSet);
+}

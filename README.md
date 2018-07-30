4689477d3769c2f9a4f1a6b0b7e65e409376df81
Rani Abu Sultan <raniabusultan@gmail.com>
2018-07-30 09:35:11 +0300

    Symbol - Initial commit

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 Typescript playground
 
-## Development server
+## Symbol
 
-Run `npm start` for a dev server. Navigate to `http://localhost:9000/`. The app will automatically reload if you change any of the source files.
-
-## TypeScript compilation in watch mode
-Run `tsc --watch` for watch in terminal.
+* We have the existing primitive types of strings `Number` `Boolean` `null` `undefined` and now we have a new primitive type known as `Symbol`.
+* The purpose of a symbol is to generate a unique ID.
+* The ID is inaccessible. But we can pass an optional symbol description while creating a symbol and we can get access to the description.
+* It doesn't matter what the description is, a Symbol is always going to create a unique ID.
+* If you want to get hold of a symbol so that you can use it again in a different or the same file, then you need to add it to the built-in symbol registry using the Symbol.for("description") method.
+* The Symbol.for("description") must have a "description" parameter. So, when this is executed it doesn't add the symbol right away. It checks first if a symbol with the same "description" is already exists in the registry. If it does exist then it will return that symbol over.
+* A practical use of symbols is using it to create unique properties in an object. So, we never have to worry about our code conflicting with existing methods or being accidently overwritten.
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
-console.log('hi');
+let s = Symbol("First Symbol - Any Description works here");
+console.log(typeof s);
+console.log(s.toString()); // caling the toString() method on the Symbol to get the description
 
-import {Car} from './Car';
+let s2 = Symbol();
+let s3 = Symbol();
 
-const car: Car = new Car();
+console.log(s2 === s3);
 
-car.go();
+let s4 = Symbol("Test");
+let s5 = Symbol("Test");
+
+console.log(s4 === s5);
+
+// Create a new Symbol and add it to the built-in symbol registry using the Symbol.for() method
+// Symbol.for("Description") checks first if a symbol with the same "Description" is already exists in the registery.
+// If it doesn't exist then it will create a new symbol in the global registry with this "Description". This what happend in line 19.
+// If it does exist then it will return that symbol with the same "Description". This what happend in line 20.
+let s6 = Symbol.for("Description");
+let s7 = Symbol.for("Description");
+
+//s6 nd s7 have the same symbol
+console.log(s6 === s7);
+
+// Get the key (the "Description") that was associated with the symbol when the symbol was added to the global registery.
+console.log(Symbol.keyFor(s6));
+
+// A practical use of symbols is using it to create unique properties in an object.
+let fname = Symbol("First Name");
+let lname = Symbol("Last Name")
+let person = {
+    [fname] : "Rani",
+    [lname] : "Abu Sultan",
+    gender: "Male"
+};
+
+console.log(Object.getOwnPropertyDescriptors(person));
+console.log(Object.getOwnPropertySymbols(person));
+console.log(Object.getOwnPropertyNames(person));
 

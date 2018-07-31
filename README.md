3ad676f3d42a6a31aab341d53b10bbfa126c2944
Rani Abu Sultan <raniabusultan@gmail.com>
2018-07-31 09:48:07 +0300

    Declarations and Annotations - Initial commit

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 Typescript playground
 
-## Development server
-
-Run `npm start` for a dev server. Navigate to `http://localhost:9000/`. The app will automatically reload if you change any of the source files.
-
-## TypeScript compilation in watch mode
-Run `tsc --watch` for watch in terminal.
+## Declarations and Annotations
+* A very important concept that applies in TypeScript is type inference.
+* We can explicitly specify the type of a variable using annotations.
+* The initialization is not mandotory.
+* A variable declared using `any` Type can hold a numeric, string, boolean, null or undefined.
+* A variable declared using `enum` Type. Enumeration are simply used to give friendlier names to numeric values and create a specific type for a variable.
+* TypeScript provide `tuples` which are special arrays that let you specify the type the array can contain.
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,48 @@
-console.log('hi');
+// TypeScript is going to infer that x is of type number (hover x).
+var x = 10;
+// TypeScript is going to infer that y is of type string (hover y).
+var y = "Hi";
+// TypeScript is going to infer that z is of type string (hover z).
+var z = true;
 
-import {Car} from './Car';
+// We can explicitly specify the type of a variable using annotations.
+var a:number = 10;
+var b:string = "Hi";
+var c:boolean = true;
 
-const car: Car = new Car();
+// The initialization is not mandotory.
+var d:number;
 
-car.go();
+// Adding number to a string will result a Concatenating and thus a string.
+var n1 = 10;
+var n2 = n1 + "Hello";
 
+// Any Type
+var info:any;
+
+// Enum Type
+enum EyeColor1 {Brown, Black, Blue}; // This declaration will assign a numeric value of 0, 1, 2 to Brown, Black , Blue
+enum EyeColor2 {Brown=1, Black=4, Blue=7}; // You can assign a numeric value specifically.
+enum EyeColor3 {Brown=1, Black, Blue}; // If you only specify Brown equal to 1, then black and blue will be assigned to 2 and 3 automaticly.
+
+var myEyeVolor = EyeColor1.Brown;
+
+console.log(myEyeVolor); // The out is numeric. So, Enumeration are simply used to give friendlier names to numeric values.
+console.log(EyeColor1[myEyeVolor]); // To get the name!
+console.log(EyeColor1[0]); // To get the name!
+
+// Array Declaration
+let strArr1:string[] = ["Hello", "World"];
+let strArr2:Array<string> = ["Hello", "World"];
+
+let anyArr1:any[] = ["Hello", 10, true];
+
+// Tuples
+let myTuple:[string, number] = ["Hello", 333]; // Declaring an array with a specific types.
+console.log(myTuple[0]);
+console.log(myTuple[1]);
+
+// Adding to a Tuple (We can add elements as long as they are of the same type specified while declaring the array)
+myTuple[2] = 100;
+console.log(myTuple[2]);
+console.log(myTuple);
